f07718a812921f82fa862bc3e78c9290803a6db0
agegold <66872114+agegold@users.noreply.github.com>
2022-04-24 23:52:37 +0900

    Update settings.cc

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -220,7 +220,7 @@ DevicePanel::DevicePanel(SettingsWindow *parent) : ListWidget(parent) {
       else
         std::system("reboot");
 
-  QPushButton *gitpull_btn = new QPushButton("업데이트:);
+  QPushButton *gitpull_btn = new QPushButton("업데이트");
   gitpull_btn->setObjectName("gitpull_btn");
   power_layout->addWidget(gitpull_btn);
 
