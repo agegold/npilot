5081f58961d91617c0c9e810591917276090629c
agegold <66872114+agegold@users.noreply.github.com>
2023-08-23 15:27:38 +0900

    Update interface.py
    staria add

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -153,6 +153,10 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 3.000
       # steering ratio according to Hyundai News https://www.hyundainews.com/assets/documents/original/48035-2022SantaCruzProductGuideSpecsv2081521.pdf
       ret.steerRatio = 14.2
+    elif candidate == CAR.STARIA:
+      ret.mass = 2280. + STD_CARGO_KG  
+      ret.wheelbase = 3.275
+      ret.steerRatio = 14.2
 
     # Kia
     elif candidate == CAR.KIA_SORENTO:
