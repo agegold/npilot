98b41c5facbfdb7d97d94981ed3bca4d85cc3141
agegold <66872114+agegold@users.noreply.github.com>
2024-05-26 02:11:41 +0900

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -282,7 +282,7 @@ class CAR(Platforms):
   HYUNDAI_STARIA_HYBRID_4TH_GEN = HyundaiCanFDPlatformConfig(
     [HyundaiCarDocs("Hyundai Staria 2024 HYBRID", "All", car_parts=CarParts.common([CarHarness.hyundai_k]))],
     CarSpecs(mass=2205, wheelbase=3.273, steerRatio=11.94),  # https://www.hyundai.com/content/dam/hyundai/au/en/models/staria-load/premium-pip-update-2023/spec-sheet/STARIA_Load_Spec-Table_March_2023_v3.1.pdf
-    flags=HyundaiFlags.HYBRID,
+    flags=HyundaiFlags.HYBRID | HyundaiFlags.RADAR_SCC,
   )
   HYUNDAI_TUCSON = HyundaiPlatformConfig(
     [
