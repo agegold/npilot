ad7f2c8338e2d4057b17bf215884192dc829464a
agegold <66872114+agegold@users.noreply.github.com>
2024-07-04 21:35:32 +0900

    Update carstate.py

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -126,10 +126,13 @@ class CarState(CarStateBase):
       ret.autoHold = cp.vl["ESP11"]["AVH_STAT"]
 
     # cruise state
-    ret.cruiseState.enabled = (cp_scc.vl["SCC12"]["ACCMode"] != 0) if not self.no_radar else \
-                                      cp.vl["LVR12"]["CF_Lvr_CruiseSet"] != 0
-    ret.cruiseState.available = (cp_scc.vl["SCC11"]["MainMode_ACC"] != 0) if not self.no_radar else \
-                                      cp.vl["EMS16"]["CRUISE_LAMP_M"] != 0
+    #ret.cruiseState.enabled = (cp_scc.vl["SCC12"]["ACCMode"] != 0) if not self.no_radar else \
+    #                                  cp.vl["LVR12"]["CF_Lvr_CruiseSet"] != 0
+    ret.cruiseState.enabled = True #bool(cp.vl["EMS16"]["CRUISE_LAMP_M"])
+    #ret.cruiseState.available = (cp_scc.vl["SCC11"]["MainMode_ACC"] != 0) if not self.no_radar else \
+    #                                  cp.vl["EMS16"]["CRUISE_LAMP_M"] != 0
+    ret.cruiseState.available = True #bool(cp.vl["EMS16"]["CRUISE_LAMP_M"])
+    
     ret.cruiseState.standstill = cp_scc.vl["SCC11"]["SCCInfoDisplay"] == 4. if not self.no_radar else False
 
     ret.cruiseState.enabledAcc = ret.cruiseState.enabled
