520c354e515bbc9a3171b72dd77f69bad3542557
agegold <66872114+agegold@users.noreply.github.com>
2024-07-04 22:04:06 +0900

    Update radard.py

--- selfdrive/controls/radard.py
+++ selfdrive/controls/radard.py
@@ -73,10 +73,10 @@ def get_lead(v_ego, ready, clusters, lead_msg, low_speed_override=True):
     cluster = None
 
   lead_dict = {'status': False}
-  if cluster is not None:
-    lead_dict = cluster.get_RadarState(lead_msg.prob)
-  elif (cluster is None) and ready and (lead_msg.prob > .5):
-    lead_dict = Cluster().get_RadarState_from_vision(lead_msg, v_ego)
+  #if cluster is not None:
+    #lead_dict = cluster.get_RadarState(lead_msg.prob)
+  #elif (cluster is None) and ready and (lead_msg.prob > .5):
+    #lead_dict = Cluster().get_RadarState_from_vision(lead_msg, v_ego)
 
   if low_speed_override:
     low_speed_clusters = [c for c in clusters if c.potential_low_speed_lead(v_ego)]
