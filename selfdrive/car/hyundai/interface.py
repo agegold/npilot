6f92033a60283afed3c57133dcebb356f6ba8dc7
agegold <66872114+agegold@users.noreply.github.com>
2024-07-07 16:57:20 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -410,10 +410,14 @@ class CarInterface(CarInterfaceBase):
       self.low_speed_alert = False
 
     buttonEvents = []
+    
+    # 크루즈 버튼 상태 변화 감지
     if self.CS.cruise_buttons != self.CS.prev_cruise_buttons:
       be = car.CarState.ButtonEvent.new_message()
       be.pressed = self.CS.cruise_buttons != 0
       but = self.CS.cruise_buttons if be.pressed else self.CS.prev_cruise_buttons
+      
+      # 버튼 타입 결정
       if but == Buttons.RES_ACCEL:
         be.type = ButtonType.accelCruise
       elif but == Buttons.SET_DECEL:
@@ -425,15 +429,21 @@ class CarInterface(CarInterfaceBase):
       else:
         be.type = ButtonType.unknown
       buttonEvents.append(be)
+
+    # 크루즈 메인 버튼 상태 변화 감지
     if self.CS.cruise_main_button != self.CS.prev_cruise_main_button:
       be = car.CarState.ButtonEvent.new_message()
       be.type = ButtonType.altButton3
       be.pressed = bool(self.CS.cruise_main_button)
       buttonEvents.append(be)
+      
+    # 감지된 버튼 이벤트 할당
     ret.buttonEvents = buttonEvents
-
+    
+    # 공통 이벤트 생성
     events = self.create_common_events(ret)
 
+    # 특정 조건에 따른 이벤트 추가
     if self.CC.longcontrol and self.CS.cruise_unavail:
       events.add(EventName.brakeUnavailable)
     #if abs(ret.steeringAngleDeg) > 90. and EventName.steerTempUnavailable not in events.events:
@@ -443,21 +453,22 @@ class CarInterface(CarInterfaceBase):
     if self.CC.turning_indicator_alert:
       events.add(EventName.turningIndicatorOn)
 
-  # handle button presses
+  # 버튼 누름 처리
     for b in ret.buttonEvents:
-      # do disable on button down
+      # 버튼 눌림 상태에 따라 이벤트 추가do disable on button down
       if b.type == ButtonType.cancel and b.pressed:
         events.add(EventName.buttonCancel)
       if self.CC.longcontrol and not self.CC.scc_live:
-        # do enable on both accel and decel buttons
+        # 가속 및 감속 버튼에서 이벤트 추가do enable on both accel and decel buttons
         if b.type in [ButtonType.accelCruise, ButtonType.decelCruise] and not b.pressed:
           events.add(EventName.buttonEnable)
+        # 충돌하는 이벤트 제거  
         if EventName.wrongCarMode in events.events:
           events.events.remove(EventName.wrongCarMode)
         if EventName.pcmDisable in events.events:
           events.events.remove(EventName.pcmDisable)
       elif not self.CC.longcontrol and ret.cruiseState.enabled:
-        # do enable on decel button only
+        # 감속 버튼에서 이벤트 추가 do enable on decel button only
         if b.type == ButtonType.decelCruise and not b.pressed:
           events.add(EventName.buttonEnable)
 
