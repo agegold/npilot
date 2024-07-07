8c8bd85c58c2b814008a1ed8c374c05069d2954a
agegold <66872114+agegold@users.noreply.github.com>
2024-07-07 19:05:49 +0900

    Update launch_chffrplus.sh

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -168,7 +168,7 @@ function launch {
   export PYTHONPATH="$PWD:$PWD/pyextra"
 
   # write tmux scrollback to a file
-  tmux capture-pane -pq -S-1000 > /data/log_$(date +%Y%m%d_%H%M%S)
+  tmux capture-pane -pq -S-1000 > /tmp/launch_log_$(date -d '+9 hour' +%Y%m%d_%H%M%S)
 
 
   # hardware specific init
