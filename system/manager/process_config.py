c51fec5931ae3dd48301e50ee76021361505a63c
agegold <66872114+agegold@users.noreply.github.com>
2025-04-21 17:30:48 +0900

    Update process_config.py

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -63,7 +63,7 @@ def and_(*fns):
 procs = [
   DaemonProcess("manage_athenad", "system.athena.manage_athenad", "AthenadPid"),
 
-  NativeProcess("loggerd", "system/loggerd", ["./loggerd"], logging),
+  #NativeProcess("loggerd", "system/loggerd", ["./loggerd"], logging),
   NativeProcess("encoderd", "system/loggerd", ["./encoderd"], only_onroad),
   NativeProcess("stream_encoderd", "system/loggerd", ["./encoderd", "--stream"], notcar),
   #PythonProcess("logmessaged", "system.logmessaged", always_run),
