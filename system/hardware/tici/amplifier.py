ef7acc043cb1b1a0414ffe9cc4b75712a548ec3b
agegold <66872114+agegold@users.noreply.github.com>
2026-01-23 02:02:11 +0900

    Update amplifier.py

--- system/hardware/tici/amplifier.py
+++ system/hardware/tici/amplifier.py
@@ -124,7 +124,7 @@ class Amplifier:
 
   def set_configs(self, configs: list[AmpConfig]) -> bool:
     # retry in case panda is using the amp
-    tries = 15
+    tries = 1
     backoff = 0.
     for i in range(tries):
       try:
