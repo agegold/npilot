5cd1c3e860948845daf18f028465fdf651b0f8ee
agegold <66872114+agegold@users.noreply.github.com>
2026-01-23 02:04:10 +0900

    Update manager.py

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -118,6 +118,10 @@ def manager_thread() -> None:
     ignore.append("pandad")
   ignore += [x for x in os.getenv("BLOCK", "").split(",") if len(x) > 0]
 
+  if params.get("HardwareC3xLite"):
+    ignore += ["micd", "soundd", "loggerd"]
+    #params.put("RecordAudio", "0")
+
   sm = messaging.SubMaster(['deviceState', 'carParams', 'pandaStates'], poll='deviceState')
   pm = messaging.PubMaster(['managerState'])
 
