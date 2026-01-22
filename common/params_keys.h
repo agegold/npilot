8622e28d52feeb03904242a7a0bd202839a23740
agegold <66872114+agegold@users.noreply.github.com>
2026-01-23 02:12:52 +0900

    Update params_keys.h

--- common/params_keys.h
+++ common/params_keys.h
@@ -143,5 +143,5 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"CarFingerprints", {PERSISTENT, STRING}},
     {"SendCarParamLogs", {PERSISTENT, STRING}},
     {"UseLanelines", {PERSISTENT, BOOL}},
-    {"HardwareC3xLite", PERSISTENT},
+    {"HardwareC3xLite", {PERSISTENT, BOOL}},
 };
