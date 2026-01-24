1493e5242146efdeedc1d7fcb3516a1a15c76d9f
agegold <66872114+agegold@users.noreply.github.com>
2026-01-25 01:06:24 +0900

    Update selfdrived.py

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -87,6 +87,10 @@ class SelfdriveD:
     self.car_state_sock = messaging.sub_sock('carState', timeout=20)
 
     ignore = self.sensor_packets + self.gps_packets + ['alertDebug']
+
+    self.camera_packets.remove("driverCameraState")
+    ignore += ['driverMonitoringState']
+
     if SIMULATION:
       ignore += ['driverCameraState', 'managerState']
     if REPLAY:
